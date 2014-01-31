bd53b7f3474155dbd5cc4b9c13eea9e7dfbb993d
hackman <hackman@ubuntu.(none)>
2014-01-31 09:48:42 +0100

    Initial commit~

---
+++ ArcEnCiel.cpp
@@ -0,0 +1,26 @@
+#include "ArcEnCiel.h"
+
+
+void ArcEnCiel::creer(Context ctxt, int num, int M, int T )
+{
+
+}
+
+void ArcEnCiel::trier()
+{
+
+}
+
+void ArcEnCiel::save( std::string name )
+{
+}
+
+void ArcEnCiel::load( std::string name )
+{
+}
+
+bool ArcEnCiel::recherche( uint64_t idx, unsigned int & p, unsigned int & q )
+{
+    return true;
+}
+
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include "Context.h"
+#include "ArcEnCiel.h"
+#include "md5.h"
+#include "sha1.h"
+
+using namespace std;
+
+int main()
+{
+
+
+     cout << "--TEST--" << endl;
+
+     cout << md5("banane") << endl;
+
+
+    return 0;
+}
