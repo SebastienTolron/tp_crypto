e53afe59233c07e82f8bda5aef48ec801efb66e5
Seb T <sebastien.tolron@gmail.com>
2014-02-07 10:54:32 +0100

    empty

--- Tp_Crypto/targetver.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// Si vous incluez SDKDDKVer.h, cela définit la dernière plateforme Windows disponible.
-
-// Si vous souhaitez générer votre application pour une plateforme Windows précédente, incluez WinSDKVer.h et
